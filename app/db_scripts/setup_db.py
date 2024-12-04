e3dbe782d53e60934f40a5070173f391c258b5ec
DP1117 <dpark50@stuy.edu>
2024-12-04 11:21:37 -0500

    db setup

---
+++ app/db_scripts/setup_db.py
@@ -0,0 +1,35 @@
+import sqlite3
+import os
+
+DB_FILE = os.path.join(os.path.dirname(__file__), "../xaea69.db")
+
+con = sqlite3.connect(DB_FILE)
+cur = con.cursor()
+
+# USER TABLE
+cur.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE
+);
+''')
+
+#FILTERS TABLE
+cur.execute('''
+CREATE TABLE IF NOT EXISTS filters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE,
+    filter TEXT NOT NULL UNIQUE
+);
+''')
+
+# WATCHLISTS TABLE
+cur.execute('''
+CREATE TABLE IF NOT EXISTS watchlists (
+    user_id INTEGER,
+    tickers TEXT NOT NULL UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+''')
