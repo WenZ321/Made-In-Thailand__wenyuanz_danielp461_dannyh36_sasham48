336a37e895b07d4a564b5a7be290031bc73968e1
DP1117 <dpark50@stuy.edu>
2024-12-11 11:31:49 -0500

    finished login

--- app/db_scripts/setup_db.py
+++ app/db_scripts/setup_db.py
@@ -54,11 +54,12 @@ def get_db_connection():
 def add_account(username, password):
     try:
         db = get_db_connection()
-        c = db.cursor()
-        c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        cur = db.cursor()
+        cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
     except sqlite3.Error as e:
         print(f"Database error: {e}")
     finally:
+        cur.close()
         db.commit()
         db.close()
     
@@ -66,16 +67,15 @@ def get_user(column, value):
     db = get_db_connection()
     cur = db.cursor()
     try:
-        query = f"SELECT * FROM users WHERE {column} = (?)"
-        cur.execute(query, (value))
+        query = f'SELECT * FROM users WHERE {column} = ?'
+        cur.execute(query, (value,))
         user = cur.fetchone()
-        print(user)
     except sqlite3.Error as e:
         print(f"An error occurred: {e}")
         user = None
     finally:
-        db.commit()
         cur.close()
+        db.commit()
         db.close()
     return user
 
